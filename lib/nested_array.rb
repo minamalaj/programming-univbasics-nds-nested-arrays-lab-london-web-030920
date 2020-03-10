273eee5fe9b5f1eb84e2e7cd4973da3369b70372
Mina Malaj <mina.malaj@hotmail.com>
2020-03-10 19:19:43 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,5 +1,5 @@
 # Examples inspired by U. S. National Organic Standards
-
+require 'pry'
 ORGANIC_PRODUCE = [
   "Strawberries",
   "Potatoes",
@@ -16,26 +16,37 @@ CONVENTIONAL_PRODUCE = [
   "Eggplant"
 ]
 
-def assembled_matrix
-  # Build an array that contains both of the above arrays
+def assembled_matrix 
+  storage_room = [
+  CONVENTIONAL_PRODUCE, 
+  ORGANIC_PRODUCE 
+] # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
 end
 
 def sorted_matrix
+storage_room = [
+  CONVENTIONAL_PRODUCE.sort, 
+  ORGANIC_PRODUCE.sort 
+] 
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
 end
 
 def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
+binding.pry
 end
 
+
+=begin
 def matrix_update(matrix, row, column, new_value)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
 end
+
+
+
